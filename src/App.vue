21b6d92f0c4e5df03855002d576fa6bd1b63f6c8
Evgeniy Dzhurabaev <even904@gmail.com>
2023-09-29 03:59:47 +0400

    fix route

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
+    <router-link to="/repa1/">Home</router-link> |
     <router-link to="/repa1/about">About</router-link> |
     <router-link to="/repa1/calendar">Calendar</router-link>
   </nav>
