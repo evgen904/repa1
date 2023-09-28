1161ac861f7752b7b0405ff15f05dea9dc5ae7f5
Evgeniy Dzhurabaev <even904@gmail.com>
2023-09-29 03:51:30 +0400

    fix url

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
-    <router-link to="/calendar">Calendar</router-link>
+    <router-link to="/repa1/about">About</router-link> |
+    <router-link to="/repa1/calendar">Calendar</router-link>
   </nav>
   <router-view />
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -12,7 +12,7 @@ const routes = [
     component: HomeView,
   },
   {
-    path: `repa1/about`,
+    path: `/repa1/about`,
     name: "about",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
@@ -21,7 +21,7 @@ const routes = [
       import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
   },
   {
-    path: `repa1/calendar`,
+    path: `/repa1/calendar`,
     name: "calendar",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
