5fa817e2a65974fda031c133ebb4c56423e49412
Evgeniy Dzhurabaev <even904@gmail.com>
2023-10-02 05:14:55 +0400

    Add module federation - Calendar

--- src/views/CalendarView.vue
+++ src/views/CalendarView.vue
@@ -33,7 +33,7 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 .calendar {
     max-width: 800px;
     background: #f0f0f0;
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,51 @@
 const { defineConfig } = require("@vue/cli-service");
+
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
-  publicPath: process.env.VUE_APP_BASEPATH,
+  publicPath:
+    process.env.NODE_ENV === "production"
+      ? process.env.VUE_APP_BASEPATH
+      : "auto",
+  chainWebpack: (config) => {
+    config
+      .plugin("module-federation-plugin")
+      .use(require("webpack").container.ModuleFederationPlugin, [
+        {
+          name: "calendar",
+          exposes: {
+            "./Calendar": "./src/views/CalendarView",
+          },
+          shared: {
+            vue: {
+              eager: true,
+              singleton: false,
+            },
+          },
+          filename: "remoteEntry.js",
+        },
+      ]);
+  },
+  configureWebpack: {
+    optimization: {
+      splitChunks: {
+        cacheGroups: {
+          defaultVendors: {
+            name: "chunk-vendors",
+            test: /[\\/]node_modules[\\/]/,
+            priority: -10,
+            chunks: "async",
+            reuseExistingChunk: true,
+          },
+          common: {
+            name: "chunk-common",
+            minChunks: 2,
+            priority: -20,
+            chunks: "async",
+            reuseExistingChunk: true,
+          },
+        },
+      },
+    },
+  },
 });
